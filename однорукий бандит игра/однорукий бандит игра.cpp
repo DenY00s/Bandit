1862d30eb22ee589fe1b0e24a2581966a8c9acd6
Gbuxty <gosha23321@gmail.com>
2023-07-14 18:01:07 +0200

    апдейт воид слот машин

--- однорукий бандит игра/однорукий бандит игра.cpp
+++ однорукий бандит игра/однорукий бандит игра.cpp
@@ -19,18 +19,34 @@ void animateSpin() {
         std::cout << "Спин..." << std::endl;
 
         // Генерация случайных символов для каждой секции барабана
-        char section1 = rand() % 5 + 'A';
+      /*  char section1 = rand() % 5 + 'A';
         char section2 = rand() % 5 + 'A';
         char section3 = rand() % 5 + 'A';
 
-        std::cout << " " << section1 << " | " << section2 << " | " << section3 << std::endl;
+        std::cout << " " << section1 << " | " << section2 << " | " << section3 << std::endl;*/
 
        /* usleep(frameDelay);*/ // Задержка между кадрами
         Sleep(1000);
         break;
     } 
 }
+int printCard(char a)
+{
+    switch (true)
+    {
+    case 'A':    return 50000; break;
+    case'K' :    return 15000; break;
+    case 'J':    return 2000; break;
+    case 'Q':    return 5000 ; break;
+    case 'Ю':   return 1000; break;
+    case '6':    return 50; break;
+    case '7':    return 100; break;
+    case '8':    return 200; break;
+    case '9':    return 500; break;
+ 
+    }
 
+}
 // Функция для игры в однорукого бандита
 void playSlotMachine() {
     std::cout << "Добро пожаловать в игру \"Однорукий бандит\"!" << std::endl;
@@ -50,38 +66,54 @@ void playSlotMachine() {
         animateSpin();
 
         // Генерация случайных символов для каждой секции барабана после анимации
-        char section1 = rand() % 3 + 'A';
-        char section2 = rand() % 3 + 'A';
-        char section3 = rand() % 3 + 'A';
+       /* char section1[] = rand() % 3 + char(-90);
+        char section2[] = rand() % 3 + char(-90);
+        char section3[] = rand() % 3 + char(-90);*/
+        char section[9] = { 'A','K','Q','J','Ю','7','6','8','9'};
+       
+        char section1 = section[rand() % 9];
+        char section2 = section[rand() % 9];
+        char section3 = section[rand() % 9];
+        std::cout << endl;
+        cout << "___________________________________________" << endl;
+        cout << "|     |                          |        |" << endl;
+        cout << "|     |                          |        |" << endl;
+        cout << "|     |                          |        |" << endl;
+        cout << "|     |                          |        |" << endl;
+        cout << "|     |                          |        |" << endl;
+        cout << "|     |" << section1<<"    " << "      " << section2 << "            " << section3 << " |" << "        |"<< endl;
+        cout << "|     |                          |        |" << endl;
+        cout << "|     |                          |        |" << endl;
+        cout << "|     |                          |        |" << endl;
+        cout << "|_____|__________________________|________|" << endl;
 
-        std::cout << std::endl;
-        std::cout << " " << section1 << " | " << section2 << " | " << section3 << std::endl;
+ 
+       /* std::cout << " " << section1 << " | " << section2 << " | " << section3 << std::endl;*/
 
-        if (section1 == section2 && section2 == section3) {
-            std::cout << "Поздравляем! Вы выиграли!" << std::endl;
+        if (section1 == section2 && section1 == section3) {
+            std::cout << "Поздравляем! Вы выиграли JackPot!" << endl;
+            int c = 0;
+            c = printCard(section1);
+             c * 10;
+             cout << "Ваш выигрышь :" << c << endl;
         }
+        else if (section1 == section2 || section1 == section3|| section2== section3) {
+            cout << "Поздравляем! Вы выиграли !" << endl;
+        }
+       
         else {
-            std::cout << "Увы, попробуйте еще раз." << std::endl;
+            cout << "Увы, попробуйте еще раз." << endl;
         }
     }
 }
 
 int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
     srand(time(0)); // Инициализация генератора случайных чисел
     setlocale(0, "");
     playSlotMachine(); // Запуск игры
 
-    cout << "___________________________________________" << endl;
-    cout << "|     |                          |        |" << endl;
-    cout << "|     |                          |        |" << endl;
-    cout << "|     |                          |        |" << endl;
-    cout << "|     |                          |        |" << endl;
-    cout << "|     |                          |        |" << endl;
-    cout << "|     |                          |        |" << endl;
-    cout << "|     |                          |        |" << endl;
-    cout << "|     |                          |        |" << endl;
-    cout << "|_____|__________________________|________|" << endl;
- 
     
     return 0;
 }
